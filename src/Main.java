cfee143337509879ecf7a01e4e3376dbf5f1f0db
raphaelming <spear131121@gmail.com>
2020-05-17 16:43:40 +0900

    1406 editor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+
 
     }
 }
---
+++ src/Stack/Baek_1406.java
@@ -0,0 +1,56 @@
+package Stack;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Baek_1406 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        Stack<Character> leftStack = new Stack<>();
+        Stack<Character> rightStack = new Stack<>();
+        for (int i = 0; i < input.length(); i++) {
+            leftStack.push(input.charAt(i));
+        }
+        int command = Integer.parseInt(br.readLine());
+        while (command-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String inputCommand = st.nextToken();
+            switch (inputCommand) {
+                case "L":
+                    if (leftStack.size() > 0) {
+                        rightStack.push(leftStack.peek());
+                        leftStack.pop();
+                    }
+                    break;
+                case "D":
+                    if (rightStack.size() > 0) {
+                        leftStack.push(rightStack.peek());
+                        rightStack.pop();
+                    }
+                    break;
+                case "B":
+                    if (leftStack.size() > 0) {
+                        leftStack.pop();
+                    }
+                    break;
+                case "P":
+                    leftStack.push(st.nextToken().charAt(0));
+                    break;
+            }
+        }
+
+        while (!leftStack.isEmpty()) {
+            rightStack.push(leftStack.peek());
+            leftStack.pop();
+        }
+        while (!rightStack.isEmpty()) {
+            bw.append(rightStack.pop());
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
