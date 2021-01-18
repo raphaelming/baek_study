4dd7c16f45814664530624b5542c4fe439e363ec
Min-Hyeok Lee <spear131121@gmail.com>
2021-01-18 14:38:03 +0900

    Create Programmers_42576.java

---
+++ src/Programmers_42576.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for (String compPlayer : completion) {
+            map.put(compPlayer, map.get(compPlayer) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+            }
+        }
+      
+        
+        return answer;
+    }
+}
